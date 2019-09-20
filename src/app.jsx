30102e68ffce04c3da6935872a35cff06a4e0cbd
Chris Torres <hwct83@gmail.com>
2019-09-19 17:49:30 -0700

    added to render element with bootstrap

--- src/app.jsx
+++ src/app.jsx
@@ -6,6 +6,43 @@ class App extends Component {
   }
 
   render() {
+    return (
+      <div className='container'>
+        <h1>Header</h1>
+        <p className='tagline'>Enter Information</p>
+        <div className='row'>
+
+          <div className='col-4'>
+            <div className='panel'>
+              <div className='panel-body'>
+                <label>How much is due?</label>
+                <input className='form-control' type='text'></input>
+                <label>How much was received?</label>
+                <input className='form-control' type='text'></input>
+                <button type='button' className='btn btn-primary'>Calculate Button</button>
+              </div>
+            </div>
+          </div>
+          <div className='col-8'>
+            <div className='outcome-alerts'>
+              <div className='alert alert-success' role='alert'></div>
+              <div className='alert alert-danger' role='alert'></div>
+              <div className='row'>
+                <div className='col-3 text-center'>Twenties</div>
+                <div className='col-3 text-center'>Ten</div>
+                <div className='col-3 text-center'>Fives</div>
+                <div className='col-3 text-center'>Ones</div>
+                <div className='col-3 text-center'>Quarters</div>
+                <div className='col-3 text-center'>Dimes</div>
+                <div className='col-3 text-center'>Nickels</div>
+                <div className='col-3 text-center'>Pennies</div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    )
   }
 }
 
